be4a9e931e27004c8ec6bc436611797441e6ed08
daytonschuh <daytonschuh@gmail.com>
2021-01-25 12:05:35 -0800

    Update TypeScriptDemo

--- Demos/TypeScriptDemo/coins.js
+++ Demos/TypeScriptDemo/coins.js
@@ -5,21 +5,24 @@ function coins(userInput) {
     // console.log("Parsed float: " + userInput2);
     var accountUser = new user;
     var userInput2 = accountUser.parseInput(userInput);
-    // truncate values
     if (userInput2 > 0) {
         userInput2 *= 100;
         console.log("After multiplying by 100: " + userInput2);
         // round the value
         userInput2 = Math.round(userInput2);
         console.log("After Math.round(): " + userInput2);
-        var d = Math.floor(userInput2 / 10); // number of dimes
+        // number of dimes
+        var d = Math.floor(userInput2 / 10);
         userInput2 %= 10;
         console.log("After dime calculation: " + userInput2);
-        var n = Math.floor(userInput2 / 5); // number of nickels
+        // number of nickels
+        var n = Math.floor(userInput2 / 5);
         userInput2 %= 5;
         console.log("After nickel calculation: " + userInput2);
-        var p = userInput2; // number of pennies
+        // number of pennies
+        var p = userInput2;
         console.log("After penny calculation: " + userInput2);
+        // DOM manipulation
         if (paragraph != null)
             paragraph.innerHTML = "Number of dimes: " + d + " + Number of nickles: " + n + " + Number of pennies: " + p;
     }
